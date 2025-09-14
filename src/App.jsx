0b7d63a9f3961a823a35e174218587d3eafee6fb
roshan-parmani <roshanparmani770@gmail.com>
2025-09-14 17:36:39 +0530

    Fix import path case issue in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './index.css'
 import { useState } from "react";
 import Mocktail from "./Components/Mocktail"
-import Search from "./Components/search";
+import Search from "./Components/Search";
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 export default function App(){
